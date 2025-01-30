045d8aa876b62a3598633ca33e10998e78b2603d
Auren009 <auren.benett@gmail.com>
2025-01-30 18:22:25 +0100

    Update WebHook.bat

--- WebHook.bat
+++ WebHook.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 
 
 :: 📌 Définition des variables
-set WEBHOOK_URL=https://discord.com/api/webhooks
+set WEBHOOK_URL=https://
 set SCREENSHOT_PATH=%TEMP%\screenshot.png
 
 set END_TIME=%TIME:~0,2%
